162713b7dcaefa6de8a2f30e6bdcdb2a6182fde0
MinniesTea <edwards.ana81@gmail.com>
2021-06-01 19:23:15 +0100

    Fizz Buzz Notes

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -7,14 +7,14 @@ public class FizzBuzz {
         counter++) {                 //This for loop prints out numbers between 1 and 100.
 
 
-      if (counter % 3 == 0 && counter % 5 == 0) {
+      if (counter % 3 == 0 && counter % 5 == 0) {       //The one with the most variables that needs to be checked goes first
         System.out.println("Fizz Buzz!");
-      } else if (counter % 3 == 0) {
+      } else if (counter % 3 == 0) {                    //else if to check the other conditions
         System.out.println("Fizz");
       } else if (counter % 5 == 0) {
         System.out.println("Buzz");
       } else {
-        System.out.println(counter);
+        System.out.println(counter);                    //Once the variables have each been checked then it can print out the rest of the numbers
       }
 
 

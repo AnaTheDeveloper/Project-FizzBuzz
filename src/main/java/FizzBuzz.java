1925ca5c2a1c0290edab75b6b3613030e20597ec
MinniesTea <edwards.ana81@gmail.com>
2021-06-01 19:21:24 +0100

    Fizz Buzz V4

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,25 +1,24 @@
 public class FizzBuzz {
 
-  //for loops version one       for( x ; x ; x ){}
+
   public void numbers() {
 
     for (int counter = 1; counter <= 100;
         counter++) {                 //This for loop prints out numbers between 1 and 100.
-      System.out.println(counter);
-      if (counter % 3 == 0) {
-        System.out.println("Fizz");
-      if (counter % 5 == 0){
-        System.out.println("Buzz");
-      if (counter % 3 == 0 && counter % 5 == 0){
+
+
+      if (counter % 3 == 0 && counter % 5 == 0) {
         System.out.println("Fizz Buzz!");
+      } else if (counter % 3 == 0) {
+        System.out.println("Fizz");
+      } else if (counter % 5 == 0) {
+        System.out.println("Buzz");
+      } else {
+        System.out.println(counter);
       }
-      }
-      }
+
+
     }
-
-
-
-
   }
 }
 
